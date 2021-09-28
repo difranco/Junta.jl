d0657593258a5448829cf00298dfb28a3ea50d5f
Anthony Di Franco <anthony@difranco.info>
2021-09-28 10:24:51 -0400

    nontrivial confidence for first set of tests now that search is sufficiently optimized

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,8 +9,8 @@ using Junta, Test
 @testset "junta" begin
     testfn = (x::BitVector) -> reduce(xor, x[[3,4]])
     dim = 4
-    ϵ = 0.01
-    error_prob = 1e-1
+    ϵ = 1e-3
+    error_prob = 1e-5
 
     t_junta = check_for_juntas_adaptive_simple(testfn, 3, ϵ, dim, error_prob)
     @test t_junta[1] == true
